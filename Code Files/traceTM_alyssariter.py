fd093a303086ec66a0c321f4b3d3ba876af9a842
AlyssaRiter23 <144466330+AlyssaRiter23@users.noreply.github.com>
2024-12-06 17:46:54 -0500

    Update traceTM_alyssariter.py

--- Code Files/traceTM_alyssariter.py
+++ Code Files/traceTM_alyssariter.py
@@ -136,8 +136,8 @@ def turing_machine_bfs(machine, input_string, max_depth=None, max_transitions=No
     # if the queue is empty and input is not accepted
     print(f"Machine Name: {machine_name}")
     file.write(f"Machine Name: {machine_name}")
-    print(f"Input not accepted in {transition_count+1} steps.")
-    file.write(f"\nInput not accepted in {transition_count+1} steps")
+    print(f"Input rejected in {transition_count+1} steps.")
+    file.write(f"\nInput rejected in {transition_count+1} steps")
     print(f"Depth of tree: {depth}")
     file.write(f"\nDepth of tree: {depth}")
     print(f"Configurations explored: {configuration_count}")  # Print explored configurations
